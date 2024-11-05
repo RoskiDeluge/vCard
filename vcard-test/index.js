f876e553ff155c911c712234d8a780d8a4bd97ff
RoskiDeluge <roberto.delgado@gmail.com>
2024-11-04 14:16:45 -1000

    First commit

--- vcard-test/index.js
+++ vcard-test/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const vCardParser = require("../parser.js");
 
-const vcardData = fs.readFileSync("contacts.vcf", "utf8");
+const vcardData = fs.readFileSync("../contacts.vcf", "utf8");
 const result = vCardParser.parse(vcardData);
 console.log(JSON.stringify(result, null, 2));
 
